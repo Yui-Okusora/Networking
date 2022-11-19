55b3fd71c38a18639e4bac897f0df509f27fabca
Yui301020 <dqdung3112@gmail.com>
2022-11-20 00:28:41 +0700

    modified:   libs/netCommon/net_message.h 	new file:   libs/netCommon/olc_net.h 	new file:   src/simpleClient/SimpleClient.cpp

--- libs/netCommon/net_message.h
+++ libs/netCommon/net_message.h
@@ -25,14 +25,40 @@ namespace olc
 
             friend std::ostream& operator<<(std::ostream& out, const message<T>& msg)
             {
-                out<<" ID: "<<int(msg.header.id)<<" Size: "<<msg.header.size;
+                out<< " ID: " << int(msg.header.id) << " Size: " << msg.header.size;
                 return out;
             }
 
             template<typename DataType>;
             friend message<T>& operator<<(message<T>& msg, const DataType& data)
             {
+                static_assert(std::is_standard_layout<DataType>::value, "Data is too complex to be pushed into vector");
 
+                size_t i = msg.body.size();
+
+                msg.body.resize(msg.body.size()+sizeof(DataType));
+
+                std::memcpy(msg.body.data() + i, &data, sizeof(DataType));
+
+                msg.header.size = msg.size();
+
+                return msg;
+            }
+
+            template<typename DataType>;
+            friend message<T>& operator>>(message<T>& msg, const DataType& data)
+            {
+                static_assert(std::is_standard_layout<DataType>::value, "Data is too complex to be pushed into vector");
+
+                size_t i = msg.body.size() - sizeof(DataType);
+
+                std::memcpy(&data, msg.body.data() + i, sizeof(DataType));
+
+                msg.body.resize(i);
+
+                msg.header.size = msg.size();
+
+                return msg;
             }
         };
     }
---
+++ src/simpleClient/SimpleClient.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "olc_net.h"
+
+enum class CustomMsgTypes : uint32_t
+{
+
+};
+
+int main(){
+    
+}
