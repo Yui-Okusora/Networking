b494ff3a8cdb48344bee7a6edf06ada3ddb232ce
Yui301020 <dqdung3112@gmail.com>
2022-11-20 02:48:23 +0700

    modified:   libs/netCommon/net_client.h 	modified:   libs/netCommon/olc_net.h 	modified:   src/simpleClient/SimpleClient.cpp

--- libs/netCommon/net_client.h
+++ libs/netCommon/net_client.h
@@ -12,21 +12,55 @@ namespace olc
         template<typename T>
         class client_interface
         {
+            client_interface() : m_socket(m_context){}
+            virtual ~client_interface()
+            {
+                Disconnect();
+            }
         public:
+
             bool Connect(const std::string& host, const uint16_t port)
             {
-                return false;
+                try
+                {
+                    m_connection = std::make_unique<connection<T>>();
+
+                    asio::ip::tcp::resolver resolver(m_context);
+                    m_endpoints = resolver.resolve(host, std::to_string(port));
+                    m_connection->ConnectToServer(m_endpoints);
+                    thrContext = std::thread([this](){ m_context.run(); });
+                }
+                catch(std::exception& e)
+                {
+                    std::cerr << "Client Exception: " << e.what() << '\n';
+                    return false;
+                }
+                return true;
             }
 
             void Disconnect()
             {
-
+                if(IsConnected())
+                {
+                    m_connection->Disconnect();
+                }
+                m_context.stop();
+                if(thrContext.joinable())
+                {
+                    thrContext -trafic
+                }
             }
 
             bool IsConnected()
             {
-
+                if(m_connection)
+                {
+                    return m_connection->IsConnected();
+                }
+                else return false;
             }
+
+            
         protected:
             asio::io_context m_context;
 
--- src/simpleClient/SimpleClient.cpp
+++ src/simpleClient/SimpleClient.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "olc_net.h"
+#include "net_connection.h"
 
 enum class CustomMsgTypes : uint32_t
 {
@@ -7,7 +8,21 @@ enum class CustomMsgTypes : uint32_t
     MovePlayer
 };
 
+class CustomClient : public olc::net::client_interface<CustomMsgTypes>
+{
+public:
+    bool FireBullet(float x, int y)
+    {
+        olc::net::message<CustomMsgTypes> msg;
+        msg.header.id = CustomMsgTypes::FireBullet;
+        msg << x << y;
+        send(msg)
+    }
+};
+
 int main(){
-    olc::net::message<CustomMsgTypes> msg;
-    msg.header.id = CustomMsgTypes::FireBullet;
+    CustomClient c;
+    c.Connect("community.onelonecoder.com",60000);
+    c.FireBullet(2.0f, 5.0f);
+    return 0;
 }
