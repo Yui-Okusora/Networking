5309b9312779381a71e0fdc2c889593ecf404223
Yui301020 <dqdung3112@gmail.com>
2022-11-19 20:54:35 +0700

    modified:   .gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
+
+#define ASIO_STANDALONE
 #include <asio.hpp>
-using namespace std;
+#include <asio/ts/buffer.hpp>
+#include <asio/ts/internet.hpp>
 
 int main(int argc, const char** argv) {
+
+    asio::error_code ec;
+
+    asio::io_context context;
+
+    asio::ip::tcp::endpoint endpoint(asio::ip::make_address("https://www.facebook.com/",ec),443);
+
+    asio::ip::tcp::socket socket(context);
+
+    socket.connect(endpoint,ec);
+
+    if(!ec){
+        std::cout<<"Connected!"<<std::endl;
+    }else{
+        std::cout<<"Failed to connect to address:\n"<<ec.message()<<std::endl;
+    }
+
     system("pause");
     return 0;
 }
