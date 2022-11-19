67fb70517a33b1e0dbc716b91b9aaa18e013a778
Yui301020 <dqdung3112@gmail.com>
2022-11-20 00:30:50 +0700

    modified:   libs/netCommon/olc_net.h 	modified:   src/simpleClient/SimpleClient.cpp

--- libs/netCommon/olc_net.h
+++ libs/netCommon/olc_net.h
@@ -1,3 +1,3 @@
 #pragma once
 #include "net_common.h"
-#include "olc_net.h"
+#include "net_message.h"
--- src/simpleClient/SimpleClient.cpp
+++ src/simpleClient/SimpleClient.cpp
@@ -3,9 +3,10 @@
 
 enum class CustomMsgTypes : uint32_t
 {
-
+    FireBullet,
+    MovePlayer
 };
 
 int main(){
-    
+    olc::net::message<CustomMsgTypes> msg;
 }
